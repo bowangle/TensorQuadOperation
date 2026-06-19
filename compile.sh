34fbd762b8e2fe635cfca0cc85fcaa84c8130e74
Bauge Angel <baugeangel@gmail.com>
2026-06-19 15:47:31 +0200

    initial commit

---
+++ compile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Paths
+PROJECT_ROOT=$(pwd)
+BUILD_DIR="$PROJECT_ROOT/build"
+
+# Clean old build
+rm -rf "$BUILD_DIR"
+
+# Configure CMake (out-of-source build)
+cmake -S "$PROJECT_ROOT" -B "$BUILD_DIR" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS_RELEASE="-Ofast -march=native"\
+
+# Build everything
+cmake --build "$BUILD_DIR" -j 4
+
+cat build/CMakeCache.txt | grep CMAKE_CXX_COMPILER
