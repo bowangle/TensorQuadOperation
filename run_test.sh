2324091f392684f884630611cab03d22b444a6dd
Bauge Angel <baugeangel@gmail.com>
2026-07-02 15:23:42 +0200

    Test result sent to output file

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-./build/Test_Mat_QR
-./build/Test_Mat_SVDDecomp
-./build/Test_Tensor3D
-./build/Test_TT_base
+./build/Test_Mat_QR      > test/test_Mat_QR.txt      2>&1 &
+./build/Test_Mat_SVDDecomp > test/test_Mat_SVDDecomp.txt 2>&1 &
+./build/Test_Tensor3D    > test/test_Tensor3D.txt    2>&1 &
+./build/Test_TT_base     > test/test_TT_base.txt     2>&1 &
+
+wait
+echo "All tests done. Output saved to individual files."
