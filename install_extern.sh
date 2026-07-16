344660a3203592abcbffe087b976689c0d6f7105
Bauge Angel <baugeangel@gmail.com>
2026-07-16 15:45:28 +0200

    move type to external library

--- install_extern.sh
+++ install_extern.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 QD_DIR="$SCRIPT_DIR/extern/qd"
 QD_INSTALL_DIR="$SCRIPT_DIR/extern/qd-install"
 TCI_QUAD_DIR="$SCRIPT_DIR/extern/tci_quad"
+TYPING_DIR="SCRIPT_DIR/extern/numeric-type-quad"
 
 echo "==> Cleaning extern/ ..."
 rm -rf "$SCRIPT_DIR"/extern/*
@@ -17,11 +18,17 @@ autoreconf -fi
 ./configure --prefix="$QD_INSTALL_DIR"
 make -j"$(nproc)"
 make install
+echo "==> QD installed to: $QD_INSTALL_DIR"
 
 echo "==> Building TCI quad:"
 cd "$TCI_QUAD_DIR"
 bash extern/clone_Eigen.sh
 bash compile.sh
 
-echo "==> QD installed to: $QD_INSTALL_DIR"
+echo "==> Building Typing:"
+cd "$TYPING_DIR"
+bash install_extern.sh
+bash compile.sh
+
+
 echo "==> Done!"
