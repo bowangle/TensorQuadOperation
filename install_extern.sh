5833ba2612526ddde93c70460e09eb8bfd009a0b
Bauge Angel <baugeangel@gmail.com>
2026-07-31 09:33:33 +0200

    switch to xfac-quad-runner using tci2

--- install_extern.sh
+++ install_extern.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-QD_DIR="$SCRIPT_DIR/extern/qd"
-QD_INSTALL_DIR="$SCRIPT_DIR/extern/qd-install"
-TCI_QUAD_DIR="$SCRIPT_DIR/extern/tci_quad"
-TYPING_DIR="$SCRIPT_DIR/extern/numeric-type-quad"
-
-echo "==> Cleaning extern/ ..."
-rm -rf "$SCRIPT_DIR"/extern/*
+XFAC_QUAD_RUNNER_DIR="$SCRIPT_DIR/extern/xfac_quad_runner"
 
 echo "==> Initializing submodules..."
 git -C "$SCRIPT_DIR" submodule update --init --recursive
 
-echo "==> Building TCI quad:"
-cd "$TCI_QUAD_DIR"
+echo "==> Building xfac_quad_runner:"
+cd "$XFAC_QUAD_RUNNER_DIR"
 bash install_extern.sh
-# bash compile.sh
 
 echo "==> Done!"
